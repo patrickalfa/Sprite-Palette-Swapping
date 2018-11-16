94580d5106110414d8185411f13764c30c0c6621
Patrick Clemer Alfarth <patrick.c.alfa@gmail.com>
2018-11-16 21:42:43 -0200

    Add sprite palettes

---
+++ Assets/Scripts/Editor/SpritePaletteEditor.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+
+namespace PTK.Palette
+{
+    using System.Collections;
+    using UnityEngine;
+    using UnityEditor;
+
+    [CustomEditor(typeof(SpritePalette))]
+    public class SpritePaletteEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            SpritePalette plt = (SpritePalette)target;
+            if (GUILayout.Button("Update palette"))
+                plt.UpdatePalette();
+        }
+    }
+}
+#endif
---
+++ Assets/Scripts/SpritePalette.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PTK.Palette
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpritePalette : MonoBehaviour
+    {
+        public Texture _palette;
+        private Material _material;
+
+        private void Start()
+        {
+            UpdatePalette();
+        }
+
+        public void SetPalette(Texture palette)
+        {
+            _palette = palette;
+            UpdatePalette();
+        }
+
+        public void UpdatePalette()
+        {
+            SetMaterial();
+
+            _material.SetTexture("_PaletteTex", _palette);
+        }
+
+        private void SetMaterial()
+        {
+            if (!_material)
+            {
+                SpriteRenderer sprt = GetComponent<SpriteRenderer>();
+                _material = new Material(Shader.Find("Sprites/Paletted"));
+                _material.SetTexture("_MainTex", sprt.sprite.texture);
+                _material.SetTexture("_PaletteTex", _palette);
+                sprt.material = _material;
+            }
+        }
+    }
+}
